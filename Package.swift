c15ee8edaead8a00cc3760dd1e6c307b28a675dd
Andy Zupko <azupko@gmail.com>
2022-06-27 12:20:28 -0500

    Version 3.5.16 Updated

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "Apptimize",
             url: "https://sdk.apptimize.com/ios/apptimize-ios-3.5.16.zip",
-            checksum: "9e65d9181d8a6de9244d9797b46768f331a4a5f3d7cff0b66485c88664f2a34d"
+            checksum: "6330a0f58815ab37697dad67de7f4b101ed1274833bbc5eb24593d5f05ec9aff"
         ),
     ]
 )
